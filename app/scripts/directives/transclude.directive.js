69993ac9df478c3ceab191b8c4a75ea2c7d2f18f
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2015-06-22 18:16:07 -0400

    using <div ng-transclude></div> instead of <ng-transclude></ng-transclude> in one place to see understand the difference

--- app/scripts/directives/transclude.directive.js
+++ app/scripts/directives/transclude.directive.js
@@ -10,9 +10,9 @@
 			restrict: "E",
 		  scope: {},
 		  transclude: true,
-		  template: "<div><b>Prepending</b><ng-transclude></ng-transclude></div>" + 
+		  template: "<div><b>Prepending</b> <div ng-transclude></div></div>" + 
 		  					"<div>This is the tutorial to understand transclude</div>" +
-		  					"<div><b>Appending</b><ng-transclude></ng-transclude></div>"
+		  					"<div><b>Appending</b> <ng-transclude></ng-transclude></div>"
 		}
 		
 		return directive;
