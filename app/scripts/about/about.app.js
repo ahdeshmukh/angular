8c435cbea67b3210ce15cf86a6b9f7a34f62b7b1
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2015-06-30 19:00:16 -0400

    creating angularApp.about module

--- app/scripts/about/about.app.js
+++ app/scripts/about/about.app.js
@@ -1,8 +1,10 @@
 (function() {
 	'use strict';
 	
+	angular.module('angularApp.about', []);
+	
 	angular
-		.module('angularApp')
+		.module('angularApp.about')
 		.config(routeConfig);
 	
 	routeConfig.$inject = ['$stateProvider', '$urlRouterProvider', 'APP_GLOBALS'];
