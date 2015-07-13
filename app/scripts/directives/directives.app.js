1080dfac288b1da282eac6258715b3855265a84d
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2015-07-13 17:15:21 -0400

    adding states for isolat scope, fixing redundant '/directives' in the url

--- app/scripts/directives/directives.app.js
+++ app/scripts/directives/directives.app.js
@@ -20,17 +20,36 @@
 			templateUrl: modulePath + 'directives.html'
 		})
 		.state('directives.transclude', {
-			url: '/directives/transclude',
+			url: '/transclude',
 			templateUrl: modulePath + 'directives.transclude.html'
 		})
 		.state('directives.dom', {
-			url: '/directives/dom',
+			url: '/dom',
 			template: '<div dom-directive>Click Me!</div>'
 		})
 		.state('directives.hyperlinks', {
-			url: '/directives/hyperlinks',
+			url: '/hyperlinks',
 			templateUrl: modulePath + 'hyperlinks/hyperlinks.view.html',
 			controller: 'HyperlinksCtrl as vm'
+		})
+		.state('directives.isolate-scope', {
+			url: '/isolate-scope',
+			templateUrl: modulePath + 'isolate-scope/isolate-scope.view.html'
+		})
+		.state('directives.isolate-scope.attr', {
+			url: 'directives/isolate-scope/attr',
+			templateUrl: modulePath + 'isolate-scope/attr/isolate-scope-attr.view.html'
+			//template: '<div>Attr</div>'
+		})
+		.state('directives.isolate-scope.equal', {
+			url: 'directives/isolate-scope/equal',
+			templateUrl: modulePath + 'isolate-scope/equal/isolate-scope-equal.view.html'
+			//template: '<div>Equal</div>'
+		})
+		.state('directives.isolate-scope.and', {
+			url: 'directives/isolate-scope/and',
+			templateUrl: modulePath + 'isolate-scope/and/isolate-scope-and.view.html'
+			//template: '<div>And</div>'
 		});
 	}
 	
